ead96b312da25db796b11be5bbbca311f9c19d7f
lokendra pandey <lokendrapandey5278@gmail.com>
2024-07-21 10:17:30 +0530

    utils setup and files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 // require('dotenv').config()
 import connectDB from './db/index.js'
 import dotenv from 'dotenv'
+
  
 dotenv.config({ 
     path:'./env'
  })
 
 connectDB()
+.then(()=> ( app.listen(process.env.PORT || 8000) , ()=>{
+    console.log(`SERVER IS RUNNING AT PORT : ${process.env.PORT }`)
+}))
+.catch((err)=> console.log("MONGODB CONNECTION FAILED !!1",err));
 
 // .then(()=>( app.listen(process.env.PORT || 8000) , () =>{
 //     console.log(`server ie running at port : ${process.env.PORT}`)
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler) => {
+    return (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).
+         catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) =>{() =>{}}
+// const asyncHandler = (fn) => async () =>{}
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
